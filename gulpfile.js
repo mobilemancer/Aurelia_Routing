ca6ed73f16c5cde903535afe517a1dedef3a9418
mobilemancer <andrkarl@gmail.com>
2015-12-06 17:08:10 +0100

    structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,34 @@
 var gulp = require('gulp'),
     ts = require('gulp-typescript'),
-    merge = require('merge');
+    merge = require('merge'),
+    flatten = require('gulp-flatten');
  
 var webroot = "wwwroot";
      
 var paths = {
-    tsSource: './aurelia_logic/**/*.ts',
-    tsOutput: "./" + webroot + "/app",
+    appSource: './app/**/*.ts',
+    appHtml: './app/**/*.html',
+    appDest: "./" + webroot + "/app",
     tsDef: "./typings/"
 };
  
 var tsCompilerConfig = ts.createProject('tsconfig.json');
  
 gulp.task('ts-compile', function () {
-    var tsResult = gulp.src(paths.tsSource)
+    var tsResult = gulp.src(paths.appSource)
         .pipe(ts(tsCompilerConfig));
  
     return merge([
         tsResult.dts.pipe(gulp.dest(paths.tsDef)),
-        tsResult.js.pipe(gulp.dest(paths.tsOutput))
+        tsResult.js.pipe(gulp.dest(paths.appDest))
     ]);
 });
  
+ gulp.task('copy-html', function(){
+    gulp.src(paths.appHtml)
+        .pipe(gulp.dest(paths.appDest)); 
+ });
+ 
 gulp.task('watch', ['ts-compile'], function () {
-    gulp.watch(paths.tsSource, ['ts-compile']);
+    gulp.watch(paths.appSource, ['ts-compile']);
 });
